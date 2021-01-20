adcefbc8df60c57369799e6fb7e462154bb0eb92
Nicolas Chanton <nicolas.chanton@gmail.com>
2021-01-20 22:56:57 +0100

    rp

--- random_proxy.py
+++ random_proxy.py
@@ -3,6 +3,8 @@ import random
 
 def random_proxy():
 
-    proxy_list = [{"http": "http://223.19.212.30:80", "https": "http://223.19.212.30:80"}, {"http": "http://1.179.185.249:8080", "https": "http://1.179.185.249:8080"}]
+    proxy_list = [
+        {"http": "http://223.19.212.30:80", "https": "http://223.19.212.30:80"},
+        {"http": "http://1.179.185.249:8080", "https": "http://1.179.185.249:8080"}]
 
     return random.choice(proxy_list)
